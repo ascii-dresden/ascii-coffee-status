d2430deca3101a09ab0934285d13dc2bc95c52fa
Lars Westermann <git@lars-westermann.de>
2022-08-19 17:06:18 +0200

    Update current table

--- client.sh
+++ client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-    curl -s "http://10.3.141.218/cm?cmnd=Status%2010" | curl -d @- -X POST -H "Content-Type: application/json" http://localhost:3000/status;
+    curl -s "http://10.3.141.218/cm?cmnd=Status%2010" | curl -d @- -X POST -H "Content-Type: application/json" https://ascii.lars-westermann.de/status;
     sleep 1;
 done;
