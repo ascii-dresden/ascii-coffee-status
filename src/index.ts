3ce46d468fbb41c0e95f81780c6a5252298d641d
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2022-08-22 20:02:14 +0200

    Make listening port configurable via PORT env

--- src/index.ts
+++ src/index.ts
@@ -86,7 +86,7 @@ app.use("/logs", express.static("./logs"), serveIndex("./logs"));
 
 app.use("/", express.static("./static"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("The application is listening on port 3000!");
 });
 
